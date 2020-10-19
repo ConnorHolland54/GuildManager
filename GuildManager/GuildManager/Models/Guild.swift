73d392d879d4f330960eed209be93fa00ea265c0
Connor Holland <supersoccergoalie123@gmail.com>
2020-10-18 18:01:22 -0600

    Updated project

---
+++ GuildManager/GuildManager/Models/Guild.swift
@@ -0,0 +1,14 @@
+//
+//  Guild.swift
+//  GuildManager
+//
+//  Created by Connor Holland on 10/18/20.
+//
+
+import Foundation
+
+struct Guild {
+    var guildName: String
+    var guildLeadership: [Player]
+    var guildMembers: [Player]
+}
---
+++ GuildManager/GuildManager/Models/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  GuildManager
+//
+//  Created by Connor Holland on 10/18/20.
+//
+
+import Foundation
+
+struct Player {
+    var name: String
+    var alliance: String?
+    var currentGuild: String?
+    var homeGuild: String?
+}
